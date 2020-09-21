7ff9ada57a72b06a8c816adc03a5207668f14e25
laureginap <laureginap@gmail.com>
2020-09-21 13:41:15 -0300

    dando andamento
    utilizando axios para efetuar a consulta http

--- desafio/src/App.js
+++ desafio/src/App.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 
 import './App.css';
 
 import Navbar from './Navbar'
+
+import Profile from './Profile'
 
 class App extends Component {
   constructor(){
@@ -12,7 +15,7 @@ class App extends Component {
         url: "https://api.github.com/users",
         client_id: "ae030cca50a42d7d37fe",
         cliente_secret: "354b04833091e243b4980c4772dc3e07e55a9e80",
-        count: 7,
+        count: 0,
         sort: "created: asc"
     },
     user: [],
@@ -20,19 +23,28 @@ class App extends Component {
     }
   }
   getUser = (e) => {
-    console.log("oi")
+    const user = e.target.value;
+    const {url, client_id, cliente_secret, count, sort } = this.state.github;
+    axios
+          .get(`${url}/${user}?client_id=${client_id}&client_secret=${cliente_secret}`
+          )
+          .then(({ data }) => this.setState({ user: data}));
+
+
   }
   render(){
-   
+    const { user } = this.state;
+    
     return  (
     <div className="App">
      <Navbar/>
      <div className="container">
        <div className="card card-body">
-         <h1>Pesquisar Usuário do GitHub</h1>
+         <h1>Pesquisar Usuário do GitHub!</h1>
          <p className="lead">Digite um nome para encontrar usuário ou repositorio</p>
          <input onChange={this.getUser} id="search" type="text" className="form-control" required></input>
        </div>
+       { user.length !== 0 ? <Profile user={user}/> : null}
      </div>
     </div>
   );
---
+++ desafio/src/Repo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Repo= () => (
+    <div className="card card-body mb-2">
+        <div className="row">
+            <div className="col-md-6">
+                <a href="" target="_blanck">{repo.name}</a>
+                
+            </div>
+        </div>
+    </div>
+    
+)
