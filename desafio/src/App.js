1292c294a7798f500a64c6cc832e875c7cde61c4
Lucas Borges <45474687+lucasborgesdev@users.noreply.github.com>
2020-10-01 23:15:33 -0300

    atualizado os components

--- desafio/src/App.js
+++ desafio/src/App.js
@@ -5,6 +5,7 @@ import Profile from "./components/Profile/Profile";
 import Repo from "./components/Repo/Repo";
 import { StyledDefault } from "./assets/styles/Global";
 
+/** 
 class App extends Component {
   constructor() {
     super();
@@ -19,30 +20,45 @@ class App extends Component {
       repos: [],
       search: "",
     };
-    //const [user, setUser] = useState([]);
-    //const [repos, setRepos] = useState([]);
-    //const [axiosTimeOut, setAxiosTimeOut] = useState ([null]);
   }
+*/
+function App() {
+  const [github, setGithub] = useState({
+    url: "https://api.github.com/users",
+    client_id: "ae030cca50a42d7d37fe",
+    client_secret: "354b04833091e243b4980c4772dc3e07e55a9e80",
+    sort: "created: asc",
+  });
 
-  getUser = (e) => {
+  const [user, setUser] = useState([]);
+  const [repos, setRepos] = useState([]);
+  const [axiosTimeOut, setAxiosTimeOut] = useState(null);
+
+  const getUser = (e) => {
     const user = e.target.value;
-    const { url, client_id, client_secret, count, sort } = this.state.github;
+    const { url, client_id, client_secret, count, sort } = github;
 
-    axios
-      .get(
-        `${url}/${user}?client_id=${client_id}&client_secret=${client_secret}`
-      )
-      .then(({ data }) => this.setState({ user: data }));
+    if (axiosTimeOut !== null) clearTimeout(axiosTimeOut);
 
-    axios
-      .get(
-        `${url}/${user}/repos?per_page=${count}&sort=${sort}&client_id=${client_id}&client_secret=${client_secret}`
-      )
-      .then(({ data }) => this.setState({ repos: data }));
+    const timeout = setTimeout(function () {
+      axios
+        .get(
+          `${url}/${user}?client_id=${client_id}&client_secret=${client_secret}`
+        )
+        .then(({ data }) => setUser(data));
+      axios
+        .get(
+          //`${url}/${user}/repos?per_page=${count}&sort=${sort}&client_id=${client_id}&client_secret=${client_secret}`
+          `${url}/${user}/repos?per_page=${count}&sort=${sort}&client_id=${client_id}&client_secret=${client_secret}`
+        )
+        .then(({ data }) => setRepos(data));
+    }, 750);
+
+    setAxiosTimeOut(timeout);
   };
 
-  renderProfile = () => {
-    const { user, repos } = this.state;
+  const renderProfile = () => {
+    //const { user, repos } = this.state;
     return (
       <div className="row">
         <div className="col-md-4">
@@ -57,34 +73,32 @@ class App extends Component {
     );
   };
 
-  render() {
-    const { user, repos } = this.state;
-    return (
-      <StyledDefault>
-        <div>
-          <Navbar />
-          <div className="container">
-            <div className="search card card-body mb-3">
-              <h1>
-                Desafio utilizando api do gitHub para buscar usuario e listar o
-                seus repositorios
-              </h1>
-              <p className="lead">
-                Digite um nome para encontrar usuários e repositórios
-              </p>
-              <input
-                onChange={this.getUser}
-                className="form-control"
-                placeholder="Digite o nome de um usuário..."
-                required
-              />
-            </div>
-            {user.length !== 0 ? this.renderProfile() : null}
+  //const { user, repos } = this.state;
+  return (
+    <StyledDefault>
+      <div>
+        <Navbar />
+        <div className="container">
+          <div className="search card card-body mb-3">
+            <h1>
+              Desafio utilizando api do gitHub para buscar usuario e listar o
+              seus repositorios
+            </h1>
+            <p className="lead">
+              Digite um nome para encontrar usuários e repositórios
+            </p>
+            <input
+              onChange={getUser}
+              className="form-control"
+              placeholder="Digite o nome de um usuário..."
+              required
+            />
           </div>
+          {user.length !== 0 ? renderProfile() : null}
         </div>
-      </StyledDefault>
-    );
-  }
+      </div>
+    </StyledDefault>
+  );
 }
 
 export default App;
